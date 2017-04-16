a34f11cad339d21a2c015427e8b0bb8b893c9bdb
John Beech <github@mkv25.net>
2017-04-16 20:14:53 +0100

    Sort out fullscreen and resize modes

--- src/js/start.js
+++ src/js/start.js
@@ -1,7 +1,7 @@
 $(function() {
 
     let logo
-    
+
     function preload() {
         game.load.image('logo', 'images/phaser.png')
     }
@@ -9,8 +9,10 @@ $(function() {
     function create() {
         logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo')
         logo.anchor.setTo(0.5, 0.5)
+        logo.scale.setTo(1.0, 1.0)
 
-        game.scale.fullScreenScaleMode = Phaser.ScaleManager.EXACT_FIT;
+        game.scale.fullScreenScaleMode = Phaser.ScaleManager.EXACT_FIT
+
         game.input.onDown.add(() => {
             toggleFullscreen(game)
         }, this);
@@ -18,6 +20,8 @@ $(function() {
         $(window).resize(function() {
             resize(game)
         });
+
+        setTimeout(resize, 500, game)
     }
 
     function update() {
@@ -25,8 +29,8 @@ $(function() {
     }
 
     function render() {
-        game.debug.cameraInfo(game.camera, 32, 32);
-        game.debug.spriteCoords(logo, 32, 500);
+        game.debug.cameraInfo(game.camera, 32, 32)
+        game.debug.spriteCoords(logo, 32, 500)
     }
 
     let game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
@@ -36,33 +40,68 @@ $(function() {
         render
     })
 
-    function toggleFullscreen(game) {
-        if (game.scale.isFullScreen) {
-            game.scale.stopFullScreen();
+    function toggleFullscreen() {
+        if (!document.fullscreenElement && // alternative standard method
+            !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement) { // current working methods
+            if (document.documentElement.requestFullscreen) {
+                document.documentElement.requestFullscreen();
+            } else if (document.documentElement.msRequestFullscreen) {
+                document.documentElement.msRequestFullscreen();
+            } else if (document.documentElement.mozRequestFullScreen) {
+                document.documentElement.mozRequestFullScreen();
+            } else if (document.documentElement.webkitRequestFullscreen) {
+                document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+            }
         } else {
-            game.scale.startFullScreen(false);
+            if (document.exitFullscreen) {
+                document.exitFullscreen();
+            } else if (document.msExitFullscreen) {
+                document.msExitFullscreen();
+            } else if (document.mozCancelFullScreen) {
+                document.mozCancelFullScreen();
+            } else if (document.webkitExitFullscreen) {
+                document.webkitExitFullscreen();
+            }
         }
     }
 
     function resize(game) {
+        let height = $(window).height()
+        let width = $(window).width()
+
         try {
-            var height = $(window).height()
-            var width = $(window).width()
             game.width = width
             game.height = height
+            console.log('Resizing to ', width, 'by', height, 'px')
             if (game.stage && game.stage.bounds) {
                 game.stage.bounds.width = width
                 game.stage.bounds.height = height
+                console.log(' - stage to ', width, 'by', height, 'px')
             }
             if (game.renderType === Phaser.WEBGL) {
                 game.renderer.resize(width, height)
+                console.log(' - webgl to ', width, 'by', height, 'px')
             }
             if (game.camera) {
                 game.camera.setSize(width, height)
-                game.camera.setPosition(width/2, height/2)
+                game.camera.setPosition(0, 0)
+                console.log(' - camera to ', width, 'by', height, 'px')
+            }
+            var canvas = document.getElementsByTagName('canvas')[0]
+            if (canvas && canvas.style) {
+                canvas.style.width = width + 'px'
+                canvas.style.height = height + 'px'
+                console.log(' - canvas to ', width, 'by', height, 'px')
             }
         } catch (ex) {
             console.log('Unable to resize game yet', ex)
         }
+
+        clearTimeout(game.resizeTimeout)
+        game.resizeTimeout = setTimeout(() => {
+            if (game.width !== width || game.height !== height) {
+                resize(game)
+            }
+        }, 0)
     }
 })
